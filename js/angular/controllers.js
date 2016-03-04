4a862669cf5ae8aa86527cf107559c098b660ec7
Marton <vetomarci@gmail.com>
2016-03-04 19:19:12 +0100

    you could send small files

--- js/angular/controllers.js
+++ js/angular/controllers.js
@@ -1,52 +1,138 @@
 var plexusControllers = angular.module('plexusControllers', [])
 
-var socket = io('http://localhost:3001')
+/*global things..*/
+var socket = io.connect('http://localhost:3001')
+var db = new PouchDB('http://localhost:5984/sdps')
 
-var peer = new window.SimplePeer({
-    initiator: true,
-    trickle: false
-})
-
-var sdp = ''
-var room = 'room' + new Date()
+/*node.js modules*/
+var fs = require('fs')
+var randomWord = require('random-word-by-length')
 
 
-plexusControllers.controller('mainCtrl', ['$scope', function ($scope) {
+plexusControllers.controller('createRoomCtrl', ['$scope', function ($scope) {
 
+    var sdp = ''
+    var room = randomWord()
+
+    var peer = new window.SimplePeer({
+        initiator: true,
+        trickle: false
+    })
+
+    /* Simple-Peer events START */
     peer.on('error', function (err) {
         console.log('error', err)
     })
 
     peer.on('signal', function (data) {
         sdp = JSON.stringify(data)
-        console.log(sdp)
-
-        console.log('room name: ' + room)
-
-        createOrJoin(room)
+        console.log('SDP and room name generated')
+        createRoom(room)
+        $scope.$apply(function () {
+            $scope.roomName = room
+        });
     })
 
-    $scope.createroom = function () {
+    peer.on('connect', function () {
+        console.log('CONNECT')
+        fs.readFile('cover.jpg', (err, filecontent) => {
 
-        console.log('location >' + location.hash + '<')
+            var fileName = 'cover_copy.jpg'
 
-        var roomname = room
+            if (err) throw err
+            var info = { fileName: fileName, fileContent: filecontent }
+            const buf = new Buffer(JSON.stringify(info))
 
-        if (location.hash === '#/createroom') {
-            createOrJoin(roomname)
-        }
+            peer.send(buf)
+            console.log('sending...')
+        })
+    })
 
+    peer.on('data', function (data) {
+        console.log('data: ' + data)
+    })   
+    /* Simple-Peer events END */
+
+
+    socket.on('pair', function (info) {
+        console.log(info)
+        db.get(room).then(function (doc) {
+            if (doc.answer) {
+                peer.signal(JSON.parse(doc.answer))
+            }
+        })
+    })
+
+    $scope.connect = function () {
+        db.get(room).then(function (doc) {
+            if (doc.answer) {
+                peer.signal(JSON.parse(doc.answer))
+            }
+        })
     }
 
-    function createOrJoin(roomName) {
-
+    function createRoom(roomName) {
         var info = {
             room: room,
             offer: sdp
         }
-
-    socket.emit('create or join', info)
-
+        socket.emit('create or join', info)
     }
 
+}])
+
+plexusControllers.controller('joinRoomCtrl', ['$scope', function ($scope) {
+
+    var roomName = ''
+    var peer = new window.SimplePeer({
+        initiator: false,
+        trickle: false
+    })
+
+    /* Simple-Peer events START */
+    peer.on('error', function (err) {
+        console.log('error', err)
+    })
+
+    peer.on('signal', function (data) {
+        sdp = JSON.stringify(data)
+        console.log('SDP and room name generated')
+        joinRoom(sdp)
+    })
+
+    peer.on('data', function (data) {
+        const buf = new Buffer(data)
+        console.log('Data captured, its length is: ' + data.length)
+
+        var dataObject = JSON.parse(buf.toString())
+
+        var fileName = dataObject.fileName
+        var fileContent = JSON.stringify(dataObject.fileContent)
+
+        const content = JSON.parse(fileContent, (key, value) => {
+            return value && value.type === 'Buffer' ? new Buffer(value.data) : value
+        })
+
+        fs.appendFile(fileName, content, (err) => {
+            if (err) throw err
+            console.log('It\'s saved!')
+        })
+    })
+    /* Simple-Peer events END */
+
+    $scope.joinRoom = function () {
+        roomName = $scope.roomName
+        console.log(roomName)
+        db.get(roomName).then(function (doc) {
+            peer.signal(JSON.parse(doc.offer))
+        })
+    }
+
+    function joinRoom(sdp) {
+        var info = {
+            room: roomName,
+            answer: sdp
+        }
+        socket.emit('create or join', info)
+    }
 }])
